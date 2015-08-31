27f6db39517f062c9392055e071cb50eb21082ee
ssangdi <ssangdi@innoaus.com>
2015-08-31 13:27:14 +0900

    new test project

---
+++ Test/src/com/innoaus/test/MainActivity.java
@@ -0,0 +1,21 @@
+package com.innoaus.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		return true;
+	}
+}
